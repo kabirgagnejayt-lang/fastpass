33fea779ef6b924725eb8359393984265ea92c17
kabirgagnejayt-lang <kabirgagnejayt@gmail.com>
2025-11-24 22:41:11 +0800

    Delete Firebase config from config.ts
    Removed public-facing Firebase configuration object.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,1 @@
 
-// NOTE: This is a public-facing configuration object.
-// No sensitive data should be stored here.
-export const firebaseConfig = {
-  apiKey: "AIzaSyBuywa1jt0qhSSoJwMN1ukUqhETVJibk2Y",
-  authDomain: "studio-4426135285-6c05e.firebaseapp.com",
-  databaseURL: "https://studio-4426135285-6c05e-default-rtdb.firebaseio.com",
-  projectId: "studio-4426135285-6c05e",
-  storageBucket: "studio-4426135285-6c05e.firebasestorage.app",
-  messagingSenderId: "1034532953101",
-  appId: "1:1034532953101:web:84d2a7d2cc317ec746c944"
-};
